7525ceda237225ed76fb6318fbc5e34b781eb9e8
Mohan Li <mohanli9807@gmail.com>
2022-11-19 15:38:21 -0800

    add PDF notes

---
+++ quick_eval.sh
@@ -0,0 +1,5 @@
+python eval.py \
+  --root_dir /home/mohan/dyn_data2/kid_run \
+  --ckpt_path /home/mohan/nsff_pl/ckpts/kid_run/epoch=99.ckpt \
+  --scene_name kid_fixtime_spiral \
+  --split test_spiral
